3bc63552619a3d5cc73163b2340ad4e6b0a1a6d7
Adam Froehlich <adamcfro@gmail.com>
2022-08-06 09:20:31 -0400

    Fixec parameter description casing

--- length-of-collatz-sequence.js
+++ length-of-collatz-sequence.js
@@ -2,8 +2,8 @@
  * This function determines how many numbers are in a chain in a 
  * Collatz Sequence, and returns the count.
  * 
- * @param {Number} num - A number parameter
- * @returns {Number} - Returns a count of the numbers in the chain
+ * @param {number} num - A number parameter
+ * @returns {number} - Returns a count of the numbers in the chain
  */
 function lengthOfCollatzSequence (num) {
   let count = 1;
